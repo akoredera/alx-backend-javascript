bc115367bfbdf0886685fd12ce91d7d28d02a2ba
akorede ridwanullahi abiodun <akoredera@gmail.com>
2024-07-24 23:49:14 +0000

    modified task 6 files

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -5,15 +5,5 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
   const signUp = signUpUser(firstName, lastName);
   const upload = uploadPhoto(fileName);
   const proAllSet = Promise.allSettled([signUp, upload]);
-  return proAllSet.then((values) => {
-    const res = [];
-    values.forEach((e) => {
-      if (e.status === 'fulfilled') {
-        res.push({ status: e.status, value: e.value });
-      } else {
-        res.push({ status: e.status, value: `${e.reason}` });
-      }
-    });
-    return res;
-  });
+  return proAllSet.then((values) => values);
 }
