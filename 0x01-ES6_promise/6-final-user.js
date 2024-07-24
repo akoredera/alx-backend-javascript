0dbf1097ac6988004aaa0a4557667290aaa99553
akorede ridwanullahi abiodun <akoredera@gmail.com>
2024-07-24 23:32:05 +0000

    Add task 6 files

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,9 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const signUp = signUpUser(firstName, lastName);
+  const upload = uploadPhoto(fileName);
+  const proAllSet = Promise.allSettled([signUp, upload]);
+  return proAllSet.then((values) => console.log(values));
+}
