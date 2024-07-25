83ad74194f1f5ce5cb0646e3b8c795cc0f9b42a0
akorede ridwanullahi abiodun <akoredera@gmail.com>
2024-07-25 21:30:34 +0000

    add task 1 files

---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,12 @@
+import ClassRoom from './0-classroom';
+
+export default function initializeRooms() {
+  const room1 = new ClassRoom(19);
+  const room2 = new ClassRoom(20);
+  const room3 = new ClassRoom(34);
+  return `[
+    ClassRoom { _maxStudentsSize: ${room1._maxStudentsSize} },
+    ClassRoom { _maxStudentsSize: ${room2._maxStudentsSize} },
+    ClassRoom { _maxStudentsSize: ${room3._maxStudentsSize} }
+  ]`;
+}
