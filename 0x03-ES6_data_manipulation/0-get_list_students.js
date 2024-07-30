d2b3d1d50b837d847b4ddd2317c19332c9b51a68
akorede ridwanullahi abiodun <akoredera@gmail.com>
2024-07-30 16:23:28 +0000

    modified file

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,6 +1,7 @@
 export default function getListStudents() {
-  return [{ id: 1, firstname: 'Guillaume', location: 'San Francisco' },
+  const arr = new Array();
+  arr.push({ id: 1, firstname: 'Guillaume', location: 'San Francisco' },
     { id: 2, firstName: 'James', location: 'Columbia' },
-    { id: 5, firstName: 'Serena', location: 'San Francisco' },
-  ];
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },)
+  return arr;
 }
