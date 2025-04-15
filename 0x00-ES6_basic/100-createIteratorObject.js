52883eb3cf8531fce40c2e4071485ab55bb97eab
Akorede Ridwanullahi Abiodun <akoredera@gmail.com>
2025-04-15 19:49:18 +0000

    modified task 100 file

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,7 +1,7 @@
 export default function createIteratorObject(report) {
-	var arr = [];
-	for (var val of Object.values(report.allEmployees)){
-		arr.push(...val);
-	}
-	return arr;
+  const arr = [];
+  for (let val of Object.values(report.allEmployees)) {
+    arr.push(...val);
+  }
+  return arr;
 }
